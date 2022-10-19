ca523b0408501be08954f0144de88906346a687e
Aurora Powell <bonzoisawesome@gmail.com>
2022-10-19 19:08:30 -0400

    conditional render on image click

--- src/components/content-components/Art.js
+++ src/components/content-components/Art.js
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
   NavLink,
+  Link
 } from "react-router-dom";
 import "../../styles/art.scss";
 
@@ -14,20 +15,37 @@ import "../../styles/art.scss";
 
 // )
 
-const IndividualPiece = ({
-  art: { title, year, medium, size, imgPath, tag },
-}) => {
-  //console.log(imgPath);
+const IndividualPiece = (
+  {art, artClick, focusedArt, setFocusedArt}) => {
+
+  const handleClick = () => {
+    focusedArt === art ? setFocusedArt(null) : artClick(art)
+    // artClick(art)
+  }
+
   return (
     <li className="art-container">
-      <img src={imgPath} />
+      <img src={art.imgPath} onClick={handleClick}/>
     </li>
   );
 };
 
+const ZoomedPiece = ({art, artClick}) => {
+
+  const handleClick = () => {
+    artClick(art)
+  }
+  return (
+    <img className="zoomed-art" src={art.imgPath} onClick={handleClick} />
+  )
+}
+
 const Art = (props) => {
   const links = [];
   const linkList = [];
+
+  // CHANGE THIS TO SET FOCUSEDART TO WHATEVER IS CLICKED
+  const [focusedArt, setFocusedArt] = useState(null);
 
   const createSidebar = (artList) => {
     // console.log(artList);
@@ -35,7 +53,7 @@ const Art = (props) => {
       if (!links.includes(art.link)) {
         links.push(art.link);
         let newLink = `/art/${art.tag}`;
-        console.log(newLink);
+        //console.log(newLink);
         linkList.push(
           <NavLink key={i + art.tag} to={newLink}>
             {art.link}
@@ -46,10 +64,36 @@ const Art = (props) => {
     return <ul>{linkList}</ul>;
   };
 
+  const artClick = (artObj) => {
+    //console.log(artObj)
+    // setFocusedArt(artObj)
+    focusedArt === artObj ? setFocusedArt(null) : setFocusedArt(artObj)
+  }
+
+
   const eachPiece = () => {
-    return props.artList.map((art, index) => {
-      return <IndividualPiece key={index} art={art} />;
-    });
+    if(!focusedArt) {
+      return props.artList.map((art, index) => {
+        return (
+          <IndividualPiece 
+            key={index} 
+            art={art} 
+            artClick={artClick}
+            focusedArt={focusedArt}
+            setFocusedArt={setFocusedArt}
+          />
+        );
+      });
+    } else {
+      return (
+        <ZoomedPiece
+          art={focusedArt}
+          artClick={artClick}
+          focusedArt={focusedArt}
+          setFocusedArt={setFocusedArt}
+        />
+      )
+    }
   };
 
   return (
@@ -57,12 +101,17 @@ const Art = (props) => {
       <div className="sidebar">
         {/* Routes here for each series, grouping. Portraits, Forms of Self-Alchemy, The Depths, etc */}
         <ul>{createSidebar(props.artList)}</ul>
+
+        {/* focusedArt ? <ArtInfo className="focused-art-info" style={bottom: 0, left: 0} */}
+        
       </div>
       <div className="content">
+        {/* {focusedArt ? <ZoomedPiece className="focused-art" art={focusedArt} artClick={artClick}/> : <ul>{eachPiece()}</ul>} */}
+        {/* {focusedArt ? <div><p>focused art set:</p><img src={focusedArt.imgPath} width="20px"/></div> : <p>bepis</p>} */}
         <ul>{eachPiece()}</ul>
       </div>
     </div>
   );
-};
+};;
 
 export default Art;
